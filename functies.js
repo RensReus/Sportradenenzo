a53cacbc7b4cbd71de036fd55bb0878307fd07f8
rensreus <rensreus@gmail.com>
2018-05-08 18:47:17 +0200

    Check if etappes compleet zijn incl aanpassing in etappe schema

--- functies.js
+++ functies.js
@@ -1,9 +1,9 @@
 const Renner = require('./app/models/renner');
 var User = require('./app/models/user');
-
-calculateUserScores = function(et){
+const Etappe = require('./app/models/etappe');
+calculateUserScores = function(et,callback){
     User.find({},function(err,users){
-        users.forEach(function(user){//get all users
+        users.forEach(function(user,index){//get all users
             var punten = 0;
             if(user.opstellingen[et-1].opstelling!=undefined){
                 Renner.find( { '_id':{$in: user.opstellingen[et-1].opstelling._id}}, function(err,renners){// get all renners in opstelling
@@ -24,11 +24,14 @@ calculateUserScores = function(et){
                     });
                     user.profieldata.poulescore.set(et-1,punten);
                     user.profieldata.totaalscore=user.profieldata.poulescore.reduce((a, b) => a + b);
-                    user.save(function(err,result) {//save score
-                        if (err) throw err;
-                    });
                 });
             };
+            user.save(function(err,result) {//save score
+                if (err) throw err;
+                if(index===users.length-1){// als laaste renner dan calculate user en continue code
+                    callback();
+                }
+            });
         });  
     });
 }
@@ -45,5 +48,19 @@ transferUsers = function(){
     });
 }
 
+transferEtappes = function(){
+    Etappe.find({},function(err,etappes){
+        etappes.forEach(function(etappe){
+            etappe.update( 
+                {'uitslagKompleet' : false },
+                {multi:true}, 
+                  function(err, numberAffected){  
+                  });
+        });
+    });
+}
+
+
 module.exports.calculateUserScores = calculateUserScores;
 module.exports.transferUsers = transferUsers;
+module.exports.transferEtappes = transferEtappes;
