d596c46e56fa83c7390810d7c0ceeca7909b04c2
rensreus <rensreus@gmail.com>
2018-05-14 00:04:30 +0200

    schedule fix again

--- index.js
+++ index.js
@@ -350,21 +350,19 @@ app.get('*', function (req, res) {
 });
 
 var resultsRule = new schedule.RecurrenceRule()
-resultsRule.minute = 53;//zodat de scrape niet stilstaat tot de volgende etappe start indien er een restart van de server is
+resultsRule.minute = 2;//zodat de scrape niet stilstaat tot de volgende etappe start indien er een restart van de server is
 var finished = false;
 
 var scrapeResults = schedule.scheduleJob(resultsRule, function () {
   console.log("scrape run at: " + new Date().toTimeString());
   Etappe.findOne({ _id: currentDisplay() }, function (err, etappe) {
-    if (!finished && !etappe.uitslagKompleet) { // als de etappe niet gefinisht is
+    if (!finished && !etappe.uitslagKompleet && displayResults(currentDisplay())) { // als de etappe niet gefinisht is
       getTimetoFinish(function (timeFinish) {// check hoe lang nog tot the finish
         finished = timeFinish[0]; // returns boolean
         resultsRule = timeFinish[1]; // returns ieder uur als de finish nog verweg is, ieder 5 min indien dichtbij en iedere min na de finish
         scrapeResults.reschedule(resultsRule);  //update new schedule
       })
     }
-    if(etappe.uitslagKompleet)
-      finished = true;
     if (finished) { // dit wordt iedere minuut na de finish uitgevoerd tot de resultaten compleet zijn
       if (!etappe.uitslagKompleet) {
         getResult(currentDisplay(), function () {
