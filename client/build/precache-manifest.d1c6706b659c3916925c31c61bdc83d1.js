a3384223dad975b2555ed610849380796ad46807
rensreus <rensreus@gmail.com>
2019-05-10 19:46:54 +0200

    client build

---
+++ client/build/precache-manifest.d1c6706b659c3916925c31c61bdc83d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d142937160fd5c6167692d0dde85130",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcb8bb9f1823bbb46b1a",
+    "url": "/static/css/main.b5c718ed.chunk.css"
+  },
+  {
+    "revision": "e66186fa60b688fb4992",
+    "url": "/static/js/2.fa92d91e.chunk.js"
+  },
+  {
+    "revision": "dcb8bb9f1823bbb46b1a",
+    "url": "/static/js/main.8d50fc36.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "252a03118724737f5219aff847d2436b",
+    "url": "/static/media/under_construction.252a0311.gif"
+  }
+]);
