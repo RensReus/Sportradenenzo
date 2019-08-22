e3ba74f1df70f3be6824b07a10e1fe2515752bbd
Bierfietsen <randombuckethorse@yahoo.com>
2019-08-22 16:17:58 +0200

    build uitschakelen poging 3

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(passport.session());
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
   const path = require("path");
   app.get("*", (req,res) => {
     res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
