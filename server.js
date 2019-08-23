97b3addd8537b6fd487e51efc72551a042633657
Bierfietsen <randombuckethorse@yahoo.com>
2019-08-23 20:03:03 +0200

    Buildfix

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require("path");
   app.get("*", (req,res) => {
-    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }else{
   app.get('*',(req,res) => {
