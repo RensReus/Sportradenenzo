38bc6c149e8712ac4a2db5e775dca780922501bd
rensreus <rensreus@gmail.com>
2021-05-26 22:27:43 +0200

    no more get client

--- src/server/db/sqlDB.js
+++ src/server/db/sqlDB.js
@@ -31,35 +31,5 @@ module.exports = {
         throw error;
       }
     }
-  },
-  getClient: (callback) => {
-    pool.connect((err, client, done) => {
-      const query = client.query.bind(client)
-
-      // monkey patch the query method to keep track of the last query executed
-      client.query = () => {
-        client.lastQuery = arguments
-        client.query.apply(client, arguments)
-      }
-
-      // set a timeout of 5 seconds, after which we will log this client's last query
-      const timeout = setTimeout(() => {
-        console.error('A client has been checked out for more than 5 seconds!')
-        console.error(`The last executed query on this client was: ${client.lastQuery}`)
-      }, 5000)
-
-      const release = (err) => {
-        // call the actual 'done' method, returning this client to the pool
-        done(err)
-
-        // clear our timeout
-        clearTimeout(timeout)
-
-        // set the query method back to its old un-monkey-patched version
-        client.query = query
-      }
-
-      callback(err, client, done)
-    })
   }
 }
