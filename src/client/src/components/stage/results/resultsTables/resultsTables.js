656d243d0c162bd47b844d913eadd17abe02614f
rensreus <rensreus@gmail.com>
2021-05-29 02:19:38 +0200

    more steamlined resultsTables

--- src/client/src/components/stage/results/resultsTables/resultsTables.js
+++ src/client/src/components/stage/results/resultsTables/resultsTables.js
@@ -3,27 +3,20 @@ import Table from '../../../shared/table'
 
 class ResultsTables extends Component {
   render() {
-    var klassementen = [];
-    for (var i = 0; i < 5; i++) {
-      if (this.props.data[i]) {
-        klassementen.push(this.props.data[i]);
-      } else {
-        klassementen.push([])
-      }
-    }
-    var classificationNamesButtons = ['Etappe', 'Algemeen', 'Punten', 'Bergen', 'Jong'];
-    var classificationNames = ['Etappe', 'Algemeen Klassement', 'Punten Klassement', 'Bergen Klassement', 'Jongeren Klassement'];
+    const classificationNamesButtons = ['Etappe', 'Algemeen', 'Punten', 'Bergen', 'Jong'];
+    const classificationNames = ['Etappe', 'Algemeen Klassement', 'Punten Klassement', 'Bergen Klassement', 'Jongeren Klassement'];
+    const classIndex = this.props.classificationIndex;
     return (
       <div className="classificationsContainer">
         <div style={{ display: 'flex' }}>
           {classificationNamesButtons.map((element, index) => {
             var buttonclassname = "klassementButton ";
-            buttonclassname += index === this.props.classificationIndex ? 'block' : 'none';
+            buttonclassname += index === classIndex ? 'block' : 'none';
             return <button style={{ display: 'block' }} disabled={this.props.stageResultsLengths[index] === 0} className={buttonclassname} key={element} onClick={this.props.changedClassificationDisplay.bind(this, index)}>{element}</button>
           })}
         </div>
         <div className="classification">
-          <Table data={klassementen[this.props.classificationIndex]} title={classificationNames[this.props.classificationIndex]} maxRows={20} classNames="classification" />
+          <Table data={this.props.data[classIndex]} title={classificationNames[classIndex]} maxRows={20} classNames="classification" />
         </div>
       </div>
     )
