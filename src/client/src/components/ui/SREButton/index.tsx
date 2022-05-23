7b06d63538b4f92ad8c1b59ec2f60a6426fcf1f1
Bierfietsen <randombuckethorse@yahoo.com>
2022-05-23 22:47:30 +0200

    Create and use SREDropdown Component. Fix errors

---
+++ src/client/src/components/ui/SREButton/index.tsx
@@ -0,0 +1,30 @@
+import './SREButton.css';
+
+type SREButtonProps = {
+  color: "blue" | "red" | "yellow" | "pink" | "gray" | "giro" | "tour" | "vuelta";
+  content?: any;
+  disabled?: boolean;
+  onClick: () => any;
+}
+
+const SREButton = (props: SREButtonProps) => {
+  return (
+    <button
+      className={getButtonClass(props)}
+      disabled={props.disabled ?? false}
+      onClick={props.onClick}
+    >
+      {props.content}
+    </button>
+  )
+}
+
+function getButtonClass(props: SREButtonProps) {
+  let classString = 'button_standard ' + props.color;
+  if (props.disabled) {
+    classString += ' disabled'
+  }
+  return classString;
+}
+
+export default SREButton
