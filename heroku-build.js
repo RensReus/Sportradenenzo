9c1d277a32d22eb55041c015736086b2352365c9
Bierfietsen <randombuckethorse@yahoo.com>
2022-06-21 23:48:18 +0200

    Change heroku build commands

--- heroku-build.js
+++ heroku-build.js
@@ -1,5 +1,13 @@
 const fs = require('fs-extra');
 
-fs.move('./src/client/build/', './build/client/build/', err => {
+fs.copy('./src/server', './build/server', err => {
+  if(err) return console.error(err);
+});
+
+fs.copy('./src/server.js', './build/server.js', err => {
+  if(err) return console.error(err);
+});
+
+fs.move('./src/client/build', './build/client/build', err => {
   if(err) return console.error(err);
 });
