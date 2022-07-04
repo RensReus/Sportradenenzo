2fdcfb09b97e9d9644019ce348a17afec9f7768e
rensreus <rensreus@gmail.com>
2022-07-04 10:40:59 +0200

    extra jaren bij manual update

--- src/client/src/components/admin/manualupdate/index.js
+++ src/client/src/components/admin/manualupdate/index.js
@@ -164,7 +164,10 @@ class manualupdate extends Component {
           <div className="grMainRow">
             <div>year</div>
             <select value={this.state.year} onChange={this.handleChangeYear}>
-              <option value="2021">2021</option>
+              <option value="2022">2022</option>
+              <option value="2023">2023</option>
+              <option value="2024">2024</option>
+              <option value="2025">2025</option>
             </select>
 
             <div>Name</div>
