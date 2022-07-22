3388619ce050163bc5f25c93df932986afdd808b
Bierfietsen <randombuckethorse@yahoo.com>
2022-07-22 22:01:16 +0200

    Update loading animation

--- src/client/src/components/shared/loadingDiv/index.js
+++ src/client/src/components/shared/loadingDiv/index.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class LoadingDiv extends Component {
     render() {
         return (
-            <div className={"loadingData " + this.props.loading} ><img src="/images/loading.gif" alt="Loading..."></img><div className="h4"></div></div>
+            <div className={"loadingData " + this.props.loading} ><img src="/images/wheel.png" alt="Loading"></img><div className="h4"></div></div>
         )
     }
 }
--- src/client/src/components/ui/SwitchButton/index.tsx
+++ src/client/src/components/ui/SwitchButton/index.tsx
@@ -1,4 +1,4 @@
-import './index.css';
+import './switchButton.css';
 
 interface SwitchButtonProps {
   action: () => {};
